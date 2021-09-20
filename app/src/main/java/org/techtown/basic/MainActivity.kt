14d2fcfaf6282c848bc3f519fe1f141ef4b8a82e
Kaygon <cwb1207@snu.ac.kr>
2021-09-20 17:49:50 +0900

    Initial commit!

---
+++ app/src/main/java/org/techtown/basic/MainActivity.kt
@@ -0,0 +1,22 @@
+package org.techtown.basic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "버튼이 눌렸습니다.", Toast.LENGTH_LONG).show()
+        }
+
+        button2.setOnClickListener {
+            val input = editTextTextPersonName.text.toString()
+            textView.setText("입력한 값: ${input}")
+        }
+    }
+}
